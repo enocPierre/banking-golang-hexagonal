e32ccb82eee0a68a8489346a7269fad693ca1baf
enocPierre <enoc.pierre@mercadolivre.com>
2022-01-30 20:09:10 -0300

    add router post

--- app/app.go
+++ app/app.go
@@ -14,9 +14,11 @@ func Start() {
 	router := mux.NewRouter()
 
 	//define routes
-	router.HandleFunc("/greet", greet)
-	router.HandleFunc("/customers", getAllCustomers)
-	router.HandleFunc("/customers/{customer_id}", getCustomer)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 
 	// starting server
 	log.Fatal(http.ListenAndServe("localhost:8000", router))
@@ -27,3 +29,8 @@ func Start() {
 		vars := mux.Vars(r)
 		fmt.Fprint(w, vars["customer_id"])
 	}
+
+	func createCustomer(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Psost rerquest received")
+	}
+
